59fa1b97cf810d4d6abcb4693c78cb2e6057bab1
CalvaryCougars <43422813+CalvaryCougars@users.noreply.github.com>
2021-01-19 08:45:57 -0500

    Update flappy-monster.js

--- flappy-monster.js
+++ flappy-monster.js
@@ -16,3 +16,42 @@ function FlappyMonster(canvas) {
 
 }
 
+FlappyMonster.prototype.start = function() {
+   // Base
+  var game = this;
+  
+  // Start Game
+  window.requestAnimationFrame(function() {
+    game.runGameLoop();
+  });
+};
+
+FlappyMonster.prototype.runGameLoop = function() {
+ switch (game.currentState) {
+    case INITIAL:
+      // DRAW INITIAL SCREEN
+      game.drawInitialScreen();
+      break;
+    case GAME_PLAYING:
+      // DRAW GAME PLAYING SCREEN
+      game.drawGamePlayingScreen();
+      break;
+    case GAME_OVER:
+      // DRAW GAME OVER SCREEN
+      game.drawGameOverScreen();
+      break;
+  }
+};
+FlappyMonster.prototype.drawInitialScreen = function() {
+  // Base
+  var game = this;
+}
+FlappyMonster.prototype.drawGamePlayingScreen = function() {
+  // Base
+  var game = this;
+}
+
+FlappyMonster.prototype.drawGameOverScreen = function() {
+  // Base
+  var game = this;
+}
