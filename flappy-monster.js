f1558ce57ce052737c33d40f9fc0b84af1e1d8f9
CalvaryCougars <43422813+CalvaryCougars@users.noreply.github.com>
2021-01-20 14:31:35 -0500

    Update flappy-monster.js

--- flappy-monster.js
+++ flappy-monster.js
@@ -3,6 +3,10 @@ var GAME_PLAYING = 2;
 var GAME_OVER = 3;
 
 
+var KEY_CODE = {
+  R: 82
+};
+
 function FlappyMonster(canvas) {
   // Base
   var game = this;
@@ -14,7 +18,56 @@ function FlappyMonster(canvas) {
   // Game State
   game.currentState = INITIAL;
 
+    // Bind Events
+  game.bindEvents();
+  
+   // Create Game Objects
+  game.createObjects();
+  
 }
+
+FlappyMonster.prototype.createObjects = function() {
+  // Base
+  var game = this;
+
+  
+};
+
+
+
+FlappyMonster.prototype.bindEvents = function() {
+  // Base
+  var game = this;
+
+  // Mouse Listener
+  game.canvas.addEventListener('click', function(event) {
+    switch (game.currentState) {
+      case INITIAL:
+        game.currentState = GAME_PLAYING;
+        break;
+      case GAME_PLAYING:
+        // DRAW GAME PLAYING SCREEN
+        break;
+    }
+  });
+
+  // Key Listener
+  window.addEventListener('keydown', function(event) {
+    switch (game.currentState) {
+      case GAME_OVER:
+        if(event.keyCode === KEY_CODE.R){
+          console.log(event.keyCode);
+          game.currentState = GAME_PLAYING;
+        }
+        break;
+    }
+  });
+};
+
+
+
+
+
 
 FlappyMonster.prototype.start = function() {
    // Base
@@ -45,6 +98,10 @@ FlappyMonster.prototype.runGameLoop = function() {
       game.drawGameOverScreen();
       break;
   }
+   window.requestAnimationFrame(function() {
+    game.runGameLoop();
+  });
+
 };
 FlappyMonster.prototype.drawInitialScreen = function() {
   // Base
@@ -88,6 +145,8 @@ FlappyMonster.prototype.drawGameOverScreen = function() {
   // Text
   game.context.fillStyle = 'white';
   game.context.font = '36px Arial';
-  game.context.fillText('GAME OVER', game.canvas.width / 2 - 100, game.canvas.height / 2);
+  game.context.fillText('Game Over :(', game.canvas.width / 2 - 100, game.canvas.height / 2);
+  game.context.font = '24px Arial';
+  game.context.fillText('Press R to Restart!', game.canvas.width / 2 - 100, game.canvas.height / 2 + 50);
 };
 
