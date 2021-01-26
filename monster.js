1a538238d46763349a7d0784f70a5d6e9dc94dce
CalvaryCougars <43422813+CalvaryCougars@users.noreply.github.com>
2021-01-26 08:21:45 -0500

    Update monster.js

--- monster.js
+++ monster.js
@@ -1,1 +1,23 @@
+function Monster(src, canvas) {
+  // Base
+  var monster = this;
+
+  // Global Attributes
+  monster.canvas = canvas;
+  monster.context = monster.canvas.getContext('2d');
+
+  // Specifications
+  monster.x = 115;
+  monster.y = 115;
+  monster.w = 115;
+  monster.h = 115;
+  monster.vy = 0;
+  monster.g = 0.10;
+  monster.src = src;
+  monster.img = null;
+  monster.frame = 0;
+
+  // Create Monster Tile Image
+  monster.create();
+}
 
